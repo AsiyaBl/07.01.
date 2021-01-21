33df3521850f2ae14f5a735c2db7eba647ecdafe
ITstart <ITstart@new.local.itstep>
2021-01-21 15:32:07 +0600

    commit

---
+++ README.md
@@ -0,0 +1,22 @@
+Site
+==========================================================
+
+>This is my first site, that i create during my education
+
+It requires for backend:
+---------------------------------------------------------
+
+1.Sequelize
+
+2.ExpressJs
+
+3.Cors
+
+
+To install them just open 'powershell' in 'backend' folder, then type this:
+
+```sh
+$ npm install expressjs
+$ npm install sequalize
+$ npm install cors
+```
---
+++ frontend/script.js
@@ -0,0 +1,39 @@
+const url = "http://localhost:3000/users";
+const vn = new Vue({
+    el:"#app",
+    data:{
+        results:[]
+    },
+    mounted(){
+        axios.get(url).then(res=>{
+            this.results=res.data
+        })
+    },
+    methods:{
+        async deleteById(index) {
+            let id = this.results[index].id
+            if(confirm('delete '+id+'?')){
+            this.results.splice(index,1)
+            await axios.delete("http://localhost:3000/users/" + id)}
+        },
+        async register(){
+            if(first_n.value === "" || last_n.value === "" || avatarka.value === "" || email.value === "" || password.value === ""){
+                alert("Заполните все данные")
+                return
+            }
+            if(check.checked===false){
+                alert("Согласитесь с нашими условиями!")
+                return
+            }
+            else{
+            await axios.post("http://localhost:3000/users",{"first_name":`${first_n.value}`,"last_name":`${last_n.value}`,"avatar":`${avatarka.value}`,"email":`${email.value}`,"password":`${password.value}`})
+            alert("пользователь создан")
+            first_n.value=null;
+            last_n.value=null;
+            avatarka.value=null;
+            email.value=null;
+            password.value=null;
+            }
+        }
+    }
+});
